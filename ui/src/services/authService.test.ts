b04424eebbbaa117c702fea3a9abf11612aff9f1
Seth Greco <sgreco@seths-laptop.lan>
2024-05-15 10:22:00 -0500

    ADD: k8s manifests - made w/ kompose. slight edits made to docker-compose and docker files.

---
+++ ui/src/services/authService.test.ts
@@ -0,0 +1,24 @@
+import { redirect } from "react-router-dom";
+import * as authMock from './authService';
+
+describe('getAuthToken', () => {
+    test('returns token when it is present in localStorage', () => {
+        const token = 'dummytoken';
+        jest.spyOn(Storage.prototype, 'getItem').mockReturnValueOnce(token);
+
+        expect(authMock.getAuthToken()).toBe(token);
+        expect(localStorage.getItem).toHaveBeenCalledWith('token');
+    })
+
+    test('returns null when token is not present in localStorage', () => {
+        jest.spyOn(Storage.prototype, 'getItem').mockReturnValueOnce('token');
+
+        expect(authMock.getAuthToken()).toBeNull();
+        expect(localStorage.getItem).toHaveBeenCalledWith('token');
+    })
+})
+
+
+jest.mock('react-router-dom', () => ({
+    redirect: jest.fn()
+}))
